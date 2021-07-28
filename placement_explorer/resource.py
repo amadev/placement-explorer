7c797d40ad9e2cdd70afbe7a3c8d6fd67fc310c8
Andrey Volkov <avolkov@mirantis.com>
2021-07-28 15:32:09 +0300

    Customize fake data

--- placement_explorer/resource.py
+++ placement_explorer/resource.py
@@ -9,7 +9,8 @@ from placement_explorer import config
 def collect():
     try:
         if config.PLACEMENT_EXPLORER_USE_FAKE_DATA:
-            return json.load(open(os.path.join(os.path.dirname(__file__), 'fake-data.json')))
+            return json.load(open(os.path.join(os.path.dirname(__file__),
+                                               config.PLACEMENT_EXPLORER_USE_FAKE_DATA)))
         else:
             clouds = [os.environ["OS_CLOUD"]]
             return {cloud: {"nodes": client.get_resources(cloud)} for cloud in clouds}
