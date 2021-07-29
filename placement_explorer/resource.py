755a48e05ad18cf8fbcfd0660193b6a1ec46896d
Andrey Volkov <avolkov@mirantis.com>
2021-07-29 12:47:12 +0300

    Make pre-commit happy

--- placement_explorer/resource.py
+++ placement_explorer/resource.py
@@ -2,15 +2,20 @@ import json
 import logging
 import os
 
-from placement_explorer import client
-from placement_explorer import config
+from placement_explorer import client, config
 
 
 def collect():
     try:
         if config.PLACEMENT_EXPLORER_USE_FAKE_DATA:
-            return json.load(open(os.path.join(os.path.dirname(__file__),
-                                               config.PLACEMENT_EXPLORER_USE_FAKE_DATA)))
+            return json.load(
+                open(
+                    os.path.join(
+                        os.path.dirname(__file__),
+                        config.PLACEMENT_EXPLORER_USE_FAKE_DATA,
+                    )
+                )
+            )
         else:
             clouds = [os.environ["OS_CLOUD"]]
             return {cloud: {"nodes": client.get_resources(cloud)} for cloud in clouds}
